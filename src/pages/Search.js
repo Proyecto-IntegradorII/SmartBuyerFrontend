88db2210b70c44e9e9b0985a6de14cc173d2025e
Lina <rivas.lina@correounivalle.edu.co>
2024-05-18 13:43:21 -0500

    Correciones de la interfaz

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,37 +1,70 @@
 import { useState } from "react";
 import './Search.css';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 function Search() {
   const [search, setSearch] = useState("")
+  const [edit, setEdit] = useState(false)
+
+  const [editIndex, setEditIndex] = useState(null);
+  const [editValue, setEditValue] = useState("");
+
+
   const handleInputChange = (e) => {
     setSearch(e.target.value);
   };
+  const handleEdit = (index,texto) => {
+    setEditIndex(index);
+    setEditValue(editValue);
+    setEdit(!edit);
 
-  const [selectedValue, setSelectedValue] = useState('');
-
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
+    if(edit)
+      {
+        const nuevaLista = [...lista]; // Copia el array original
+        nuevaLista[index] = editValue; // Actualiza el valor del elemento editado
+        setLista(nuevaLista); // Actualiza el estado con la nueva lista
+       
+      }
+    console.log(index)
   };
+
+
+  const handleDelete = (index) => {
+    const nuevaLista = [...lista]; // Crear una copia del array original
+    nuevaLista.splice(index, 1); // Eliminar el elemento en el índice dado
+    setLista(nuevaLista); // Actualizar el estado con la nueva lista
+  };
+  
+  const [lista, setLista] = useState(["carne", "pollo"]);
+
 
     return (
       <div className="Search">
         <header className="Search-header">
         <img src="/images/logo.png" className="Search-logo" alt="logo" />
-        <select value={selectedValue} onChange={handleChange} className="list-control rounded-pill" >
-        <option value="">Seleccionar busqueda guardada</option>
-          <option value="opcion1">Opción 1</option>
-          <option value="opcion2">Opción 2</option>
-          <option value="opcion3">Opción 3</option>
-        </select>
           <input
               value={search}
               type="text"
-              placeholder="Nueva busqueda"
-              className="form-control rounded-pill" 
+              className="form-contro" 
               onChange={handleInputChange}
               style={{ marginRight: '10px' }}
           />
-        <button  className="boton" type="submit"    style={{ margin: '50px', padding: 0 }} >Buscar</button>
+          <p>Tu lista actualmente se ve así:</p>
+          {lista.map((texto, index) => (
+            <div key={index} className="input-with-icon">
+            <input
+              type="text"
+              className="form-list"
+              value={editIndex === index ? editValue : texto}
+              readOnly={!edit}
+              onChange={(e) => setEditValue(e.target.value)}
+              style={{ marginBottom: '1vw', marginLeft: '4vw' }}
+            />
+            <FaEdit className="icon" onClick={() => handleEdit(index, texto)} style={{ marginLeft: '1vw' }} />
+            <FaTrash className="icon" onClick={() => handleDelete(index)} style={{ marginLeft: '1vw' }}/>
+          </div>
+          ))}
+        <button  className="boton" type="submit"    style={{ margin: '1vw', padding: 0 }} >Buscar</button>
    
         </header>
       </div>
