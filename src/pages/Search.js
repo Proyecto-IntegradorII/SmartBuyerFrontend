f396ea8c94bce351c3a70cf44f34fe43d294a868
David Moreno <davidan7845@gmail.com>
2024-05-21 07:24:27 -0500

    tailwind

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -13,18 +13,17 @@ function Search() {
   const handleInputChange = (e) => {
     setSearch(e.target.value);
   };
-  const handleEdit = (index,texto) => {
+  const handleEdit = (index, texto) => {
     setEditIndex(index);
     setEditValue(editValue);
     setEdit(!edit);
 
-    if(edit)
-      {
-        const nuevaLista = [...lista]; // Copia el array original
-        nuevaLista[index] = editValue; // Actualiza el valor del elemento editado
-        setLista(nuevaLista); // Actualiza el estado con la nueva lista
-       
-      }
+    if (edit) {
+      const nuevaLista = [...lista]; // Copia el array original
+      nuevaLista[index] = editValue; // Actualiza el valor del elemento editado
+      setLista(nuevaLista); // Actualiza el estado con la nueva lista
+
+    }
     console.log(index)
   };
 
@@ -34,24 +33,25 @@ function Search() {
     nuevaLista.splice(index, 1); // Eliminar el elemento en el índice dado
     setLista(nuevaLista); // Actualizar el estado con la nueva lista
   };
-  
+
   const [lista, setLista] = useState(["carne", "pollo"]);
 
 
-    return (
-      <div className="Search">
-        <header className="Search-header">
+  return (
+    <div className="Search">
+      
+      <header className="Search-header">
         <img src="/images/logo.png" className="Search-logo" alt="logo" />
-          <input
-              value={search}
-              type="text"
-              className="form-contro" 
-              onChange={handleInputChange}
-              style={{ marginRight: '10px' }}
-          />
-          <p>Tu lista actualmente se ve así:</p>
-          {lista.map((texto, index) => (
-            <div key={index} className="input-with-icon">
+        <input
+          value={search}
+          type="text"
+          className="form-contro"
+          onChange={handleInputChange}
+          style={{ marginRight: '10px' }}
+        />
+        <p>Tu lista actualmente se ve así:</p>
+        {lista.map((texto, index) => (
+          <div key={index} className="input-with-icon">
             <input
               type="text"
               className="form-list"
@@ -61,14 +61,14 @@ function Search() {
               style={{ marginBottom: '1vw', marginLeft: '4vw' }}
             />
             <FaEdit className="icon" onClick={() => handleEdit(index, texto)} style={{ marginLeft: '1vw' }} />
-            <FaTrash className="icon" onClick={() => handleDelete(index)} style={{ marginLeft: '1vw' }}/>
+            <FaTrash className="icon" onClick={() => handleDelete(index)} style={{ marginLeft: '1vw' }} />
           </div>
-          ))}
-        <button  className="boton" type="submit"    style={{ margin: '1vw', padding: 0 }} >Buscar</button>
-   
-        </header>
-      </div>
-    );
-  }
-  
-  export default Search;
+        ))}
+        <button className="boton" type="submit" style={{ margin: '1vw', padding: 0 }} >Buscar</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default Search;
