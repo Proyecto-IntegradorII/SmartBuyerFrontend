20b6e7b85d688fe6c9844a219e77727f7072d024
KPaola-M74 <kampao74@gmail.com>
2024-05-26 19:51:40 -0500

    implementacion de grabacion y permiso del microfono y rutas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,73 @@
-import { useState } from "react";
+import React, { useState, useRef } from 'react';
 import { FaEdit, FaTrash, FaMicrophone } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
-import AudioRecorder from "./audio";
 function Search() {
   const [search, setSearch] = useState("");
   const [edit, setEdit] = useState(false);
   const [editIndex, setEditIndex] = useState(null);
   const [editValue, setEditValue] = useState("");
   const [lista, setLista] = useState(["carne", "pollo"]);
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState(null);
+  const [error, setError] = useState(null);
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
+  
+  const handleStartRecording = async () => {
+    try {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorderRef.current = mediaRecorder;
+  
+    mediaRecorder.ondataavailable = (event) => {
+    audioChunksRef.current.push(event.data);
+    };
+  
+    mediaRecorder.onstop = async () => {
+    const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/webm' });
+    const audioUrl = URL.createObjectURL(audioBlob);
+    setAudioUrl(audioUrl);
+    audioChunksRef.current = [];
+    await sendAudioToServer(audioBlob);
+    };
+    mediaRecorder.start();
+    setIsRecording(true);
+    setError(null);  // Limpiar cualquier error previo
+    } catch (error) {
+    console.error('Error accessing microphone', error);
+    setError('No se pudo acceder al micrófono. Por favor, otorga permisos para usar el micrófono.');
+    }
+    };
+  
+    const handleStopRecording = () => {
+    if (mediaRecorderRef.current) {
+    mediaRecorderRef.current.stop();
+    setIsRecording(false);
+    }
+    };
+  
+    const sendAudioToServer = async (audioBlob) => {
+    try {
+    const formData = new FormData();
+    formData.append('audio', audioBlob, 'recording.webm'); 
+  
+    const response = await fetch('http://localhost:9000/transcribes', {
+    method: 'POST',
+    body: formData
+    });
+  
+    if (!response.ok) {
+    throw new Error('Error al enviar el audio al servidor');
+    }
+  
+    const data = await response.json();
+    console.log('Respuesta del servidor:', data);
+    setSearch(data.transcripcion);
+    } catch (error) {
+    console.error('Error al enviar el audio', error);
+    setError('Error al enviar el audio al servidor');
+    }
+    };
 
   const handleInputChange = (e) => {
     setSearch(e.target.value);
@@ -19,9 +79,9 @@ function Search() {
     setEdit(!edit);
 
     if (edit) {
-      const nuevaLista = [...lista];
-      nuevaLista[index] = editValue;
-      setLista(nuevaLista);
+    const nuevaLista = [...lista];
+    nuevaLista[index] = editValue;
+    setLista(nuevaLista);
     }
   };
 
@@ -33,27 +93,30 @@ function Search() {
 
   return (
     <div className="text-center">
-      <header className="flex flex-col items-center justify-center">
-        <img src="/images/logo.png" className="mt-20 w-80" alt="logo" />
-        <FaUser 
-          className="absolute top-2 right-2 text-2xl cursor-pointer" 
-          style={{ marginTop: '3vw' }} 
-        />
+    <header className="flex flex-col items-center justify-center">
+    <img src="/images/logo.png" className="mt-20 w-80" alt="logo" />
+    <FaUser 
+    className="absolute top-2 right-2 text-2xl cursor-pointer" 
+    style={{ marginTop: '3vw' }} 
+    />
 
-        <div className="relative mt-8"  style={{marginTop: '3vw', marginBottom: '20px'}}>
+    <div className="relative mt-8"  style={{marginTop: '3vw', marginBottom: '20px'}}>
 
-          <input
-            value={search}
-            type="text"
-            className="form-contro w-96 h-32 border border-orange-600 bg-gray-300 text-xl text-center rounded-lg"
-            onChange={handleInputChange}
-          />
-          <AudioRecorder    />
-        </div>
-        <p className="text-xl mt-4">Tu lista actualmente se ve así:</p>
-        {lista.map((texto, index) => (
-          <div key={index} className="flex items-center mt-4" style={{ marginRight: '-5vw' }}>
-            <input
+    <input
+    value={search}
+    type="text"
+    className="form-contro w-96 h-32 border border-orange-600 bg-gray-300 text-xl text-center rounded-lg"
+    onChange={handleInputChange}
+    />
+    <div onClick={isRecording ? handleStopRecording : handleStartRecording}>
+    <FaMicrophone style={{ transform: 'translateX(350px)', marginTop: '-3vw' }} />
+    {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+    </div>
+    <p className="text-xl mt-4">Tu lista actualmente se ve así:</p>
+    {lista.map((texto, index) => (
+    <div key={index} className="flex items-center mt-4" style={{ marginRight: '-5vw' }}>
+    <input
               type="text"
               className="form-list w-96 h-10 border border-orange-600 bg-gray-300 text-xl text-center rounded-lg"
               value={editIndex === index ? editValue : texto}
