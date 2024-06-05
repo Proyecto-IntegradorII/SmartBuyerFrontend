809b35ae3f9b84ff59a18afb885c2cf3f9f71241
Lina <rivas.lina@correounivalle.edu.co>
2024-06-05 12:58:29 -0500

    pruebas

--- src/test/Search.test.js
+++ src/test/Search.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor, act } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom'; // Importa BrowserRouter
+import fetchMock from 'jest-fetch-mock';
 
 import Search from '../pages/Search';
+
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
+
+
 
 const { getByPlaceholderText, getByText , getByDisplayValue, queryByText, getByRole } = render(<Router>
   <Search />
@@ -80,3 +87,54 @@ expect(editButton).toHaveAttribute('aria-label', 'Save');
 fireEvent.click(editButton);
 expect(editButton).toHaveAttribute('aria-label', 'Edit');
 });
+
+
+test('envía texto de búsqueda a OpenIA y actualiza la lista', async () => {
+  const { getByPlaceholderText, getByRole, queryByText } = render(<Router>
+    <Search />
+  </Router>);
+  
+  // Ingresa el texto en el textarea
+  const textarea = getByPlaceholderText('Azúcar blanca 2.5kg, Café Aguila Roja 500 gramos, Leche bolsa 6 unidades 1 ml Alpina...');
+
+  fireEvent.change(textarea, { target: { value: 'Azúcar blanca 2.5kg' } });
+
+  // Simular el clic en el botón "Analizar compras"
+  fireEvent.click(getByRole('button', { name: /Analizar compras/i }));
+  
+  // Esperar a que la lista se actualice
+  await waitFor(() => {
+    // Verificar que los nuevos elementos están en la lista
+    expect(getByText('Azúcar blanca 2.5kg')).toBeInTheDocument();
+  });
+});
+
+/*test('sends audio recording to server and updates search input', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({ transcripcion: 'Azúcar blanca 2.5kg' }));
+
+  const { getByText } = render(
+    <Router>
+      <Search />
+    </Router>
+  );
+
+  const startRecordingButton = getByText('Start Recording');
+  fireEvent.click(startRecordingButton);
+
+  await waitFor(() => {
+    const stopRecordingButton = getByText('Stop Recording');
+    fireEvent.click(stopRecordingButton);
+  });
+
+  await waitFor(() => {
+    expect(fetchMock).toHaveBeenCalledWith(
+      'https://smart-buyer-bf8t.onrender.com/transcribes',
+      expect.any(Object)
+    );
+  });
+
+  await waitFor(() => {
+    const textarea = getByPlaceholderText('Azúcar blanca 2.5kg, Café Aguila Roja 500 gramos, Leche bolsa 6 unidades 1 ml Alpina...');
+    expect(textarea.value).toBe('Azúcar blanca 2.5kg');
+  });
+});*/
---
+++ src/test/request.test.js
@@ -0,0 +1,100 @@
+import fetchMock from 'jest-fetch-mock';
+import { postData, postLogin, postLoginGoogle } from '../conections/requests';
+
+// Enable fetch mocks before each test
+beforeEach(() => {
+    fetchMock.resetMocks();
+});
+
+describe('API Requests', () => {
+    test('postData submits data successfully', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ message: 'Success' }), { status: 200 });
+
+        const response = await postData({ username: 'testuser', email: 'test@example.com' });
+
+        expect(response).toBe('Data submitted successfully');
+        expect(fetchMock).toHaveBeenCalledWith('https://smart-buyer-bf8t.onrender.com/register/user', expect.any(Object));
+    });
+
+    /*test('postData handles error response', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ error: 'Email or Username already taken' }), { status: 400 });
+
+        const response = await postData({ username: 'testuser', email: 'test@example.com' });
+
+        expect(response).toBe('Email or Username already taken');
+    });
+
+    test('postData handles network error', async () => {
+        fetchMock.mockRejectOnce(new Error('Network Error'));
+
+        const response = await postData({ username: 'testuser', email: 'test@example.com' });
+
+        expect(response).toBe('Conection failed');
+    });*/
+
+    test('postLogin handles successful login', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({
+            token: 'fake-token',
+            usuarioData: {
+                email: 'test@example.com',
+                id_usuario: 1,
+                nombre_usuario: 'testuser',
+                // additional user data fields
+            },
+        }), { status: 200 });
+
+        const response = await postLogin({ username: 'testuser', password: 'password' });
+
+        expect(response).toBe('Inicio de sesión exitoso');
+        expect(localStorage.getItem('token')).toBe('fake-token');
+    });
+
+    /*test('postLogin handles incorrect credentials', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ error: 'Credenciales incorrectas' }), { status: 401 });
+
+        const response = await postLogin({ username: 'testuser', password: 'wrongpassword' });
+
+        expect(response).toBe('Credenciales incorrectas');
+    });
+
+    test('postLogin handles network error', async () => {
+        fetchMock.mockRejectOnce(new Error('Network Error'));
+
+        const response = await postLogin({ username: 'testuser', password: 'password' });
+
+        expect(response).toBe('Error de conexión');
+    });*/
+
+    test('postLoginGoogle handles successful login', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({
+            token: 'fake-token',
+            usuarioData: {
+                email: 'test@example.com',
+                id_usuario: 1,
+                nombre_usuario: 'testuser',
+                // additional user data fields
+            },
+        }), { status: 200 });
+
+        const response = await postLoginGoogle({ token: 'google-token' });
+
+        expect(response).toBe('Inicio de sesión exitoso');
+        expect(localStorage.getItem('token')).toBe('fake-token');
+    });
+
+    /*test('postLoginGoogle handles incorrect credentials', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({ error: 'Credenciales incorrectas' }), { status: 401 });
+
+        const response = await postLoginGoogle({ token: 'google-token' });
+
+        expect(response).toBe('Credenciales incorrectas');
+    });
+
+    test('postLoginGoogle handles network error', async () => {
+        fetchMock.mockRejectOnce(new Error('Network Error'));
+
+        const response = await postLoginGoogle({ token: 'google-token' });
+
+        expect(response).toBe('Error de conexión');
+    });*/
+});
